6c89ddba5e46e9ade44eeb51a2e028f94ea52827
jprtenorio00 <jp-tenorio@hotmail.com>
2024-04-20 11:49:24 -0300

    DB Script - Modificações

--- db-joaotenorio.sql
+++ db-joaotenorio.sql
@@ -1,2 +1,3 @@
 CREATE DATABASE joaotenorio;
 SELECT * FROM joaotenorio.company;
+SELECT joaotenorio.GAMES.title, joaotenorio.COMPANY.name FROM joaotenorio.GAMES INNER JOIN joaotenorio.VIDEOGAMES ON joaotenorio.GAMES.id_console = joaotenorio.VIDEOGAMES.id_console INNER JOIN joaotenorio.COMPANY ON joaotenorio.VIDEOGAMES.id_company = joaotenorio.COMPANY.id_company
