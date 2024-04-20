cdba37805846c5fcdb3a2c5fba1e7b15b31c34b3
jprtenorio00 <jp-tenorio@hotmail.com>
2024-04-20 11:56:58 -0300

    Questão 4 - Modificações II

--- q4_JoaoTenorio.py
+++ q4_JoaoTenorio.py
@@ -8,15 +8,17 @@ from q3_JoaoTenorio import execute, cursor, db
 
 os.system('cls')
 
-generate_inner_join = lambda tables, on_conditions: tables[0] + "".join([" INNER JOIN " + tables[i] + " ON " + on_conditions[i-1] for i in range(1, len(tables))])
+generate_inner_join = lambda tables, on_conditions: tables[0] + "".join(
+    [" INNER JOIN " + tables[i] + " ON " + on_conditions[i-1] for i in range(1, len(tables))]
+)
 
-tables = ["GAMES", "VIDEOGAMES", "COMPANY"]
-on_conditions = ["GAMES.id_console = VIDEOGAMES.id_console", "VIDEOGAMES.id_company = COMPANY.id_company"]
-inner_join_query = generate_inner_join(tables, on_conditions)
-attributes = ["GAMES.title", "COMPANY.name"]
-select_query = f"SELECT {', '.join(attributes)} FROM {inner_join_query}"
+tables = lambda: ["GAMES", "VIDEOGAMES", "COMPANY"]
+on_conditions = lambda: ["GAMES.id_console = VIDEOGAMES.id_console", "VIDEOGAMES.id_company = COMPANY.id_company"]
+attributes = lambda: ["GAMES.title", "COMPANY.name"]
+inner_join_query = lambda: generate_inner_join(tables(), on_conditions())
+select_query = lambda: f"SELECT {', '.join(attributes())} FROM {inner_join_query()}"
 
-print("Professor estou só debugando kkk: ", select_query)
-execute(select_query)
+print("Professor estou só debugando kkk: ", select_query())
+execute(select_query())
 
 db.close()
